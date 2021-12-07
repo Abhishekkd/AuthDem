4a84174abd444c39b864d3163435e2b1848bd1a5
Abhishekkd <ad7679@srmist.edu.in>
2021-12-07 16:34:02 +0530

    ++  login middleware,refactor, model methods

--- index.js
+++ index.js
@@ -50,15 +50,17 @@ app.post('/register',async(req,res)=>{
     //but we are not going to save password instead we are gonna use bcrypt which will make us a hashed password
     //which we will store in our database
     const{password,username}= req.body;
-    const hash = await bcrypt.hash(password,12);
-    //storing these data in our database
-    const user = new User({
-        //username gonna be same but we'll be saving hashed password
-        username,
-        password:hash
-    })
+    // const hash = await bcrypt.hash(password,12);
+    // //storing these data in our database
+    // const user = new User({
+    //     //username gonna be same but we'll be saving hashed password
+    //     username,
+    //     password:hash
+    // })
+    const user = new User({username,password})
     //after successfully logging in we'll add your user_id to session
     await user.save();
+    req.session.user_id=user._id;
     res.redirect('/');
 })
 
@@ -69,12 +71,13 @@ app.get('/login',(req,res)=>{
 //submitting login data
 app.post('/login',async(req,res)=>{
     const {username,password} = req.body;
-    //we'll find the user with this username as we dont have any id and thats why username should always be unique
-   const user = await User.findOne({username}) ;// it means where ({username:username})
+    // we'll find the user with this username as we dont have any id and thats why username should always be unique
+//    const user = await User.findOne({username}) ;// it means where ({username:username})
     //after finding what we wanna do is compare the password that we have in the req.body to the hashed password on this user
-    const validPassword = await bcrypt.compare(password,user.password);
-    if(validPassword){
-        req.session.user_id = user._id;
+//     const validPassword = await bcrypt.compare(password,user.password);
+    const foundUser = await User.authenticate(username,password); 
+    if(foundUser){
+        req.session.user_id = foundUser._id;
         res.redirect('/secret')
     } else{
         res.redirect('/login')
@@ -86,6 +89,7 @@ app.post('/logout',(req,res)=>{
     req.session.user_id = null;
     res.redirect('/login')
 })
+//so both these routes are protected by this middleware
 
 //basic route which eventually going to be secret 
 app.get('/secret',requireLogin,(req,res)=>{
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 //this model will only have a username and password and both will be strings
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     username:{
         type:String,
@@ -12,6 +12,27 @@ const userSchema = new mongoose.Schema({
         type:String,
         required: [ true,'Password cannot be blank']
     }
+})
+//static method on our model
+//for authenticating credentials
+userSchema.statics.authenticate = async function(username,password){
+    //find the user by its username
+    //this refers to the particular model or particular schema which will be particular user(instance of our model)
+    const foundUser = await this.findOne({username}); //cant use this "this.findOne" with arrow function
+    
+    //if found then validate and compare using bcrypt
+   const isValid =  await bcrypt.compare(password,foundUser.password); //return a boolean
+   //if is valid is true ,we return the found user object(entire user) otherwise false
+   return isValid ? foundUser : false;
+
+}
+//to run tis function pre saving something
+//mongoose middleware
+userSchema.pre('save',async function(next){
+    //so if only user name is changed we wont we rehashing the password
+    if(!this.isModified('password')) return next(); 
+    this.password = await bcrypt.hash(this.password,12);
+    next();//next is going to call save() which then will save our hashed password
 })
 
 module.exports = mongoose.model('User', userSchema);
